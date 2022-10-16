6d8d959eb74bb9424d61bfa8ff9aec21554660ab
Ivan <67783938+monsterveaf@users.noreply.github.com>
2022-10-16 20:15:14 +0300

    Inline queries added

--- Main.py
+++ Main.py
@@ -3,9 +3,7 @@ import telegram.ext as tge
 import requests
 import datetime
 
-
 weather_token = '22d38e4b789796c92e6e4fe65846ff67'
-
 
 # Create a connection with bot
 updater = tge.Updater(token='5780766591:AAFyzLIU3DGshE5kHsOYJro5O3Je-yF09Fg')
@@ -13,6 +11,7 @@ dispatcher = updater.dispatcher
 
 
 def get_weather(city, token):
+    """ Receive city and return weather information"""
 
     try:
         request = requests.get(
@@ -20,43 +19,40 @@ def get_weather(city, token):
         )
         json_data = request.json()
         weather_desc = json_data["weather"][0]["main"]
-        curr_temp = json_data["main"]["temp"]
-        max_temp = json_data["main"]["temp_max"]
-        min_temp = json_data["main"]["temp_min"]
-        feels_temp = json_data["main"]["feels_like"]
+        curr_temp = int(json_data["main"]["temp"])
+        max_temp = int(json_data["main"]["temp_max"])
+        min_temp = int(json_data["main"]["temp_min"])
+        feels_temp = int(json_data["main"]["feels_like"])
         humidity = json_data["main"]["humidity"]
         pressure = json_data["main"]["pressure"]
-        wind_speed = json_data["wind"]["speed"]
+        wind_speed = int(json_data["wind"]["speed"])
 
         return f"Date and time: {datetime.datetime.now().strftime('%m/%d/%Y, %H:%M:%S')}\n" \
                f"Your location: {city}\n" \
                f"Weather description: {weather_desc}\n" \
-               f"Current temperature: {curr_temp}\n" \
-               f"Minimal temperature: {min_temp}\n" \
-               f"Maximal temperature: {max_temp}\n" \
-               f"Temperature feels like: {feels_temp}\n" \
-               f"Humidity: {humidity}\n" \
-               f"Pressure: {pressure}\n" \
-               f"Wind speed: {wind_speed}"
-
+               f"Current temperature: {curr_temp}° С\n" \
+               f"Minimal temperature: {min_temp}° С\n" \
+               f"Maximal temperature: {max_temp}° С\n" \
+               f"Temperature feels like: {feels_temp}° С\n" \
+               f"Humidity: {humidity}%\n" \
+               f"Pressure: {pressure} hPa\n" \
+               f"Wind speed: {wind_speed} m/s"
 
     except KeyError:
-        return "Please, check name of a city you entered"
+        return "Please, check the name of a city you entered"
 
 
 def start(update: tg.Update, context: tge.CallbackContext):
-
     """ Send message, when the bot is started
         or received a /start command"""
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=f"Hello, {update.effective_chat.first_name}, jan.\n"
                                                                     "I can tell the weather today, ebana.\n"
-                                                                    "Please, send me the ciy i tuda priedut bratki."  
-                                                                    "Bistro nahui blyat!\n")
+                                                                    "Please, send me the ciy i tuda priedut bratki.\n"
+                                                                    "Bistro nahui blyat!")
 
 
 def weather(update: tg.Update, context: tge.CallbackContext):
-
     """ Receive location and sends weather """
 
     location = update.message.text
@@ -65,10 +61,28 @@ def weather(update: tg.Update, context: tge.CallbackContext):
 
 
 def unknown(update: tg.Update, context: tge.CallbackContext):
-
     """ Receive unknown command and send warning """
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, moya tvoya ne ponimat'.")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, moya tvoya ne ponimat")
+
+
+def inline_caps(update: tg.Update, context: tge.CallbackContext):
+    """ Receive inline_query and send weather info """
+
+    query = update.inline_query.query
+
+    if not query:
+        return "hui"
+
+    results = []
+    results.append(
+        tg.InlineQueryResultArticle(
+            id=query.upper(),
+            title='Weather',
+            input_message_content=tg.InputTextMessageContent(get_weather(query, weather_token))
+        )
+    )
+    context.bot.answer_inline_query(update.inline_query.id, results)
 
 
 if __name__ == '__main__':
@@ -85,6 +99,9 @@ if __name__ == '__main__':
     message_handler = tge.MessageHandler(tge.Filters.text, weather)
     dispatcher.add_handler(message_handler)
 
+    # Create a inline queries
+    inline_caps_handler = tge.InlineQueryHandler(inline_caps)
+    dispatcher.add_handler(inline_caps_handler)
+
     # Start the bot
     updater.start_polling()
-
